dfd5d40ee18250039e4654346794eb8487326046
TheDreadPirate <solidsnake1298@gmail.com>
2025-10-30 17:43:01 -0400

    Cleanup

--- OlderInfo/scratchFiles/importJSONfromURL.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import json
-
-sourceJsonURL = 'https://api.nhk.or.jp/nhkworld/epg/v6/world/all.json?apikey=EJfK8jdS57GqlupFgAfAAwr573q01y6k'
-with urllib.request.urlopen(sourceJsonURL) as url:
-    data = json.load(url)
-with open('downloadedjson.json', 'w') as jsonfile:
-    json.dump(data, jsonfile)
